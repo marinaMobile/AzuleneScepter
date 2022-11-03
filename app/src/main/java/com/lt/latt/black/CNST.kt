7933731600846a275fddea256a3a934aa25f3ecf
Marina <marina@Marinas-MacBook-Pro.local>
2022-11-03 13:07:51 +0300

    first commit

---
+++ app/src/main/java/com/lt/latt/black/CNST.kt
@@ -0,0 +1,18 @@
+package com.lt.latt.black
+
+object CNST {
+    const val AF_DEV_KEY = "rELN5x4uBkzup4JUWW2P45"
+    const val jsoupCheck = "3f2c"
+    const val ONESIGNAL_APP_ID = "772f4107-50d6-4c7b-b0bc-20ddc1d5cb66"
+
+    var lru = "http://azulenescepter.xyz/go.php?to=1&"
+
+
+    val odone = "sub_id_1="
+    val twoSub = "sub_id_2="
+
+    var MAIN_ID: String? = ""
+    var C1: String? = "c11"
+    var D1: String? = "d11"
+    var DEV: String = "false"
+}
